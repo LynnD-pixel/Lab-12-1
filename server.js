ffe70f7b8e973e2ab01e3eee32a03f3bbd7a9dca
Lynn <diamondsforever2016@gmail.com>
2026-03-12 10:32:16 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const  express = require("express");
+const path = require("path");
+
+const app = express();
+const port = 3000;
+
+//Route for homepage
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+//Route for contact page
+app.get("/contact", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "contact.html"));
+});
+
+//Start server
+app.listen(port, () => {
+    console.log(`Server is runnung on port ${port}`);
+});
